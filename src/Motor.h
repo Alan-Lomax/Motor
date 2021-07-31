58b013c993a7cc5741adc37953ac467722091c21
Alan-Lomax <87946633+Alan-Lomax@users.noreply.github.com>
2021-07-31 18:21:49 -0400

    Initial work in progress load

--- src/Motor.h
+++ src/Motor.h
@@ -5,21 +5,23 @@
 class Motor {
   // All local variables begin with underscore to differentiate them from other external variables   
   private:
+    enum direction {CLOCKWISE, COUNTERCLOCKWISE};
+    
     byte _motorPin1;
     byte _motorPin2; 
-  	byte _motorSpeedPin; 
+    byte _motorSpeedPin; 
     byte _speedValue; 
     bool _currentDirCW;
-	
+    void init();
+    
   public:
     Motor(byte MotorPin1, byte MotorPin2, byte MotorSpeedPin);
-    void init();
     void DirCW();
     void DirCCW();
     void On(byte SpeedVal);
-	void Off();
+    void Off();
     bool IsDirCW();
-	bool IsDirCCW();
+    bool IsDirCCW();
     byte CurrentSpeed();
 };
 #endif
