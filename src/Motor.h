896e73c219048a50bd35f3758558ad0ca2c7419a
Alan-Lomax <87946633+Alan-Lomax@users.noreply.github.com>
2021-08-03 17:04:24 -0400

    Update Motor.h

--- src/Motor.h
+++ src/Motor.h
@@ -21,8 +21,8 @@ class Motor {
     Motor(byte MotorPin1, byte MotorPin2, byte MotorSpeedPin);     // Instantiate a motor using two control pins and one speed pin (latter must be a PWM capable pin)
     direction getDirection();                                      // Returns one of the two enumarations (CLOCKWISE or COUNTER_CLOCKWISE) for current motor direction. 
     void setDirection(direction newDirection);                     // Sets current motor direction to one of the two enumarations (CLOCKWISE or COUNTER_CLOCKWISE)
-    void On(byte SpeedVal);                                        // Turns on the motor by setting the motor speed (0-255)
-    void Off();                                                    // Turns off the motor (same as setting motor speed to 0)
+    void on(byte SpeedVal);                                        // Turns on the motor by setting the motor speed (0-255)
+    void off();                                                    // Turns off the motor (same as setting motor speed to 0)
     byte currentSpeed();                                           // Returns the current motor speed (0-255)
 };
 #endif
